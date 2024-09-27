58bb017e537bf49a47595688bfa413c95bad6330
Eryk Dąbek <eryk.dabek@euvic.pl>
2024-09-27 21:31:57 +0200

    Adjust imports

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import { Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Link } from "expo-router"
+import { Component } from "@components"
 
 const Index = () => {
   return (
     <SafeAreaView>
       <Text className="">Index</Text>
       <Link href={{ pathname: "/(tabs)/home" }}>Home</Link>
+      <Component />
     </SafeAreaView>
   )
 }
